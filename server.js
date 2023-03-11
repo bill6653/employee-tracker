ad469069b80173c7ff1ffcabf4553d3e0874a19c
bill6653 <billcrs512@gmail.com>
2023-03-10 22:59:20 -0600

    a

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const connection = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'Vicarious2!',
+        password: 'tipper70',
         database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
